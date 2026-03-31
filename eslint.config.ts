0f62c631dafdae8c8ddad4df56d08ef591056f33
Marcin Falkowski <marcin@MacBook-Air-2.local>
2026-03-31 12:50:06 +0200

    Lint

---
+++ eslint.config.ts
@@ -0,0 +1,14 @@
+import globals from "globals";
+import tseslint from "typescript-eslint";
+import { defineConfig } from "eslint/config";
+
+export default defineConfig([
+  {
+    ignores: ["dist/**", "node_modules/**"],
+  },
+  {
+    files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
+    languageOptions: { globals: globals.node },
+  },
+  tseslint.configs.recommended,
+]);
